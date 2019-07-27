166f578390282bdff12bb381893f440d11b34fba
Pavel Turanchoks <ipuncho@gmail.com>
2019-07-27 18:12:48 +0300

    swiper spring

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Swiper from './Swiper';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Swiper
+      cards={[
+        {
+          name: 'Heather',
+          age: 32,
+          distance: '4 miles away',
+          text:
+            "My nickname is Gilette because I'm the best a man can get. Also I will cut you.",
+          correct: true,
+        },
+      ]}
+    />
   );
 }
 
