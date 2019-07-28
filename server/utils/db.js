ba52a242510238a8443e9020aa409b8f387a8dc0
PainKiller <dmitry_malugin@hotmail.com>
2019-07-28 13:21:04 +0300

    добавил db.js

---
+++ server/utils/db.js
@@ -0,0 +1,22 @@
+
+const MongoClient = require('mongodb').MongoClient;
+const keys = require('../config');
+
+let db;
+
+const loadDB = async () => {
+    if (db) {
+        return db;
+    }
+    try {
+        const client = await MongoClient.connect(keys.mongoURI);
+        global.db = client.db('mongo');
+        db = client.db('mongo');
+        console.log('connection to db successful');
+    } catch (err) {
+        console.log('connection to db failed ', err);
+    }
+    return db;
+};
+
+module.exports = loadDB;
