860232fb89dab734fe9b1f98497bb04050256cc6
kostenko-anton <kostenko-anton@users.noreply.github.com>
2019-07-28 10:15:01 +0300

    use users endpoint

--- web/src/Results.js
+++ web/src/Results.js
@@ -1,82 +1,53 @@
-import React from 'react';
+import React from "react";
 import sha1 from "js-sha1";
-import './Results.css';
-
+import { useAxiosRequest } from "use-axios-request";
+import "./Results.css";
 
 const me = {
-  email: 'aaaa@gmail.com',
-  name: 'Никола Абрамов',
-  score: 65,
-}
-
-const users = [
-  {
-    email: 'test@gmail.com',
-    name: 'Семен Алексеевич',
-    score: 20,
-  },
-  {
-    email: 'test2@gmail.com',
-    name: 'Петр',
-    score: 10,
-  },
-  {
-    email: 'test4@gmail.com',
-    name: 'Владислав Иванов',
-    score: 7,
-  },
-  {
-    email: 'test4@gmail.com',
-    name: 'Никита Григорьевич',
-    score: 6,
-  },
-  {
-    email: 'test4@gmail.com',
-    name: 'Иван Саломонович',
-    score: 5,
-  },
-  {
-    email: 'test@gmail.com',
-    name: 'Лука Валентинович',
-    score: 5,
-  },
-  {
-    email: 'test2@gmail.com',
-    name: 'Петро',
-    score: 2,
-  },
-]
+  email: "aaaa@gmail.com",
+  name: "Никола Абрамов",
+  score: 65
+};
 
 function Results() {
+  const { isFetching, data } = useAxiosRequest("/users");
+  if (isFetching) return null;
   return (
     <div className="results">
-      {/* <h2>Таблица лидеров</h2> */}
+      <h2>Таблица лидеров</h2>
       <div className="results__inner">
         <div className="results__my">
           <img
-            src={`https://robohash.org/${sha1(me.name, me.email)}.png?bgset=bg1`}
+            src={`https://robohash.org/${sha1(
+              me.name,
+              me.email
+            )}.png?bgset=bg1`}
             alt={me.name}
           />
           <span className="results__user-name results__my-name">{me.name}</span>
-          <span className="results__user-score results__my-score">{me.score}</span>
+          <span className="results__user-score results__my-score">
+            {me.score}
+          </span>
         </div>
         <div className="results__table">
-          {users.map((user, i) => {
+          {data.map((user, i) => {
             return (
               <div className="results__user">
                 <div className="results__user-info">
-                  <span className="results__user-place">{i+1}</span>
+                  <span className="results__user-place">{i + 1}</span>
                   <img
-                    src={`https://robohash.org/${sha1(user.name, user.email)}.png?bgset=bg1`}
+                    src={`https://robohash.org/${sha1(
+                      user.name,
+                      user.email
+                    )}.png?bgset=bg1`}
                     alt={user.name}
                   />
                   <span className="results__user-name">{user.name}</span>
                 </div>
-                <span className="results__user-score ">{user.score}</span>
+                <span className="results__user-score ">{user.points}</span>
               </div>
             );
           })}
-          
         </div>
       </div>
     </div>
